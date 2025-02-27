9f7b6ba1200c446ff3cde77a045e0d1c8b44d1ec
Annie McMahon <108587699+AnnieMcMahon@users.noreply.github.com>
2025-02-27 12:19:31 -0500

    fixed merge conflicts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,8 @@
-<<<<<<< HEAD
 import { cn } from '@/lib/utils';
 import type { Metadata } from 'next';
 import './globals.css';
-=======
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 import NavigationBar from "@/app/_components/navbar/NavigationBar";
 import Footer from "./_components/footer/footer";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
->>>>>>> origin/main
 
 export const metadata: Metadata = {
   title: 'Classics Revisited',
@@ -32,16 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-<<<<<<< HEAD
-      <body className={cn('mx-auto flex min-h-screen max-w-7xl antialiased')}>
-=======
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={cn('mx-auto min-h-screen max-w-7xl antialiased')}>
         <NavigationBar />
->>>>>>> origin/main
         {children}
-
         <Footer />
       </body>
     </html>
