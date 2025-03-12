d0b0d58055e5bc29899ba7e30e69164a1c846341
Annie McMahon <108587699+AnnieMcMahon@users.noreply.github.com>
2025-03-12 14:49:37 -0400

    still trying to fix footer logo on iPhone after deployment

--- app/_components/Footer/Footer.tsx
+++ app/_components/Footer/Footer.tsx
@@ -4,24 +4,28 @@ import { navigationData, galleryData } from '@/app/data/navbarData';
 
 const Footer = () => {
   return (
-    <footer className="flex items-center justify-between bg-black p-10 text-white transition-all sm:p-14">
+    <footer className="flex items-center justify-between bg-black p-10 text-white transition-all">
       <div className="flex items-center gap-8 md:gap-24">
-        <Link href="/#hero" className="flex w-24 h-24 justify-center overflow-hidden rounded-full bg-white">
-        
+        <Link
+          href="/#hero"
+          className="relative flex h-24 w-24 items-center justify-center rounded-full bg-white"
+        >
           <Image
             src="/images/logos/FullLogo_Transparent.png"
-            className="object-cover"
-            width={400}
-            height={400}
+            layout="fill"
+            objectFit="contain"
             alt="logo"
           />
         </Link>
 
-
         <div className="flex flex-col">
           {navigationData.map((item, index) => {
             return (
-              <Link key={index} className="text-sm/6 hover:text-neutral-400" href={item.path}>
+              <Link
+                key={index}
+                className="text-sm/6 hover:text-neutral-400"
+                href={item.path}
+              >
                 {item.name}
               </Link>
             );
@@ -30,7 +34,11 @@ const Footer = () => {
         <div className="flex flex-col">
           {galleryData.map((item, index) => {
             return (
-              <Link key={index} className="text-sm/6 hover:text-neutral-400" href={item.path}>
+              <Link
+                key={index}
+                className="text-sm/6 hover:text-neutral-400"
+                href={item.path}
+              >
                 {item.name}
               </Link>
             );
