f62309da20423a0b45ccd68c7916f2b53bc84e05
Annie McMahon <108587699+AnnieMcMahon@users.noreply.github.com>
2025-04-27 18:39:14 -0400

    updated hero

--- app/_components/Hero/Hero.tsx
+++ app/_components/Hero/Hero.tsx
@@ -2,20 +2,31 @@ import Link from "next/link";
 
 export default function Hero() {
   return (
-    <div className="border px-8 pt-16 flex flex-col lg:flex-row md:align-center" id="hero">
-      <div className="lg:w-[60%] order-first lg:order-last">
-        <img src="images/woodworking/woodworking1.jpeg" alt="Hero Image" className="mb-8 lg:mb-0" />
-      </div>
-      <div className="lg:w-[40%]">
-        <h1 className="text-2xl/10 font-bold md:text-[2rem] lg:mt-24">
-          Where Timeless Design <br/> Meets Modern Living
+    <div
+      className="relative w-full h-[80vh] overflow-hidden flex items-start justify-start pt-32 pl-16"
+      id="hero"
+    >
+      <div
+        className="absolute inset-0 bg-cover bg-center animate-zoom"
+        style={{ backgroundImage: "url('/images/woodworking/woodworking1.jpeg')" }}
+      ></div>
+
+      <div className="relative z-10 bg-black bg-opacity-40 p-8 rounded-lg shadow-lg max-w-80">
+        <h1 className="text-4xl mb-4 font-cursive text-center text-white">
+          Where Timeless Design <br /> Meets Modern Living
         </h1>
 
-        <p className="lg:w-[80%] lg:pr-8 lg:py-12 py-4 text-lg">
-        At Classics Revisited, we believe that the past and present can coexist beautifully, and we&apos;re here to make sure your home reflects both history and modern comfort.
+        <p className="text-lg mb-6 text-center text-white">
+          At Classics Revisited, we believe that the past and present can coexist beautifully, and we're here to make sure your home reflects both history and modern comfort.
         </p>
-        <div className="my-8">
-        <Link href="/#contact" className="px-4 py-4 bg-black text-white font-bold text-sm rounded-lg hover:bg-gray-600 transition">Contact Us Today</Link>
+
+        <div className="text-center">
+          <Link
+            href="/#contact"
+            className="inline-block px-6 py-3 bg-white text-black font-bold text-sm rounded-lg hover:bg-gray-600 transition"
+          >
+            Contact Us Today
+          </Link>
         </div>
       </div>
     </div>
