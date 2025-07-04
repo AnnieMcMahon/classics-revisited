1ac3ee9b4571d7c39c63f5adc3424c2b10fd888a
Annie McMahon <108587699+AnnieMcMahon@users.noreply.github.com>
2025-07-04 10:59:18 -0400

    updated footer logo and color

--- app/_components/Footer/Footer.tsx
+++ app/_components/Footer/Footer.tsx
@@ -4,14 +4,14 @@ import { navigationData, galleryData } from '@/app/data/navbarData';
 
 const Footer = () => {
   return (
-    <footer className="flex items-center justify-between bg-black p-10 text-white transition-all">
+    <footer className="flex items-center justify-between bg-gray-900 p-10 text-white transition-all">
       <div className="flex items-center gap-8 md:gap-24">
         <Link
           href="/#hero"
-          className="relative flex h-24 w-24 items-center justify-center rounded-full bg-white"
+          className="relative flex h-36 w-36 items-center justify-center"
         >
           <Image
-            src="/images/logos/FullLogo_Transparent.png"
+            src="/images/logos/new_fulllogo_transparent.png"
             layout="fill"
             objectFit="contain"
             alt="logo"
