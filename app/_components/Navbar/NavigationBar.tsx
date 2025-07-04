27d8825cfb86481e546070d286f6c3a3e433a9a5
Annie McMahon <108587699+AnnieMcMahon@users.noreply.github.com>
2025-07-04 10:50:26 -0400

    changed top logo and nav bar colors

--- app/_components/Navbar/NavigationBar.tsx
+++ app/_components/Navbar/NavigationBar.tsx
@@ -13,19 +13,19 @@ interface NavProps {
 const NagivationBar = ({ children }: NavProps) => {
   return (
     <SidebarProvider>
-      <nav className="fixed top-0 z-50 w-full bg-white p-3 shadow-md md:p-0">
+      <nav className="fixed top-0 z-50 w-full bg-gray-900 p-3 shadow-md md:p-1">
         <div className="container mx-auto flex w-[100%] items-center justify-between py-5 pl-1 pr-2 lg:py-6">
-          <div className="w-full text-xl font-bold text-gray-800">
+          <div>
             <Link
               href="/#hero"
-              className="absolute -top-2 left-0 m-0 lg:-top-1"
+              className="absolute py-2 -top-2 left-0 m-0 lg:-top-1"
               style={{ fontWeight: '0' }}
             >
               <Image
                 alt="a renovation company"
-                width={100}
-                height={100}
-                src="/images/logos/FullLogo_Transparent.png"
+                width={120}
+                height={120}
+                src="/images/logos/new_fulllogo_transparent.png"
                 className="object-contain"
               />
             </Link>
@@ -37,7 +37,7 @@ const NagivationBar = ({ children }: NavProps) => {
                 <Link
                   href={item.path}
                   key={index}
-                  className="font-light text-gray-500 transition-colors hover:text-gray-900"
+                  className="font-light text-white transition-colors hover:text-gray-300"
                 >
                   {item.name}
                 </Link>
